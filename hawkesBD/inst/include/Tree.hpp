e51238b369a417e7e111aa108704f9338c99fbd9
slwu89 <slwu89@berkeley.edu>
2018-11-26 13:04:05 -0800

    midway through poisson bd process

--- hawkesBD/inst/include/Tree.hpp
+++ hawkesBD/inst/include/Tree.hpp
@@ -11,26 +11,32 @@
 /* C++ includes */
 #include <iostream>
 #include <vector>
+#include <memory>
+#include <set>
 
-/* forward declare node class */
+/* forward declare node & PRNG class */
 class Node;
+class prng;
 
 /* tree class */
 class Tree {
 public:
   
-              Tree();
+              Tree(double lambda, double mu, double duration, std::unique_ptr<prng> prng_);
               ~Tree();
   void        listNodes();
   
   std::vector<Node*>& getTraversalOrder(){ return postOrderSequence; }
   
-private:
+protected:
   
   /* data */
   Node*               root;
   std::vector<Node*>  nodes;
   std::vector<Node*>  postOrderSequence;
+  
+  /* PRNG */
+  std::unique_ptr<prng> prngPtr;
   
   /* functions */
   void                initializeTraversalOrder();
--- hawkesBD/src/Tree.cpp
+++ hawkesBD/src/Tree.cpp
@@ -7,14 +7,52 @@
 
 #include "Tree.hpp"
 #include "Node.hpp"
+#include "PRNG.hpp"
 
 /* constructor & destructor */
-Tree::Tree() : 
-  root(nullptr)
+Tree::Tree(double lambda, double mu, double duration, std::unique_ptr<prng> prng_) : 
+  root(nullptr),
+  prngPtr(std::move(prng_))
 {
   std::cout << "tree born at " << this << std::endl;
   
-  initializeTraversalOrder();
+  /* generate a birth-death process with parameter lambda, mu, and duration */
+  
+  /* initialize the single lineage, adding the descendant to a list of active nodes */
+  nodes.push_back(new Node);
+  nodes.push_back(new Node);
+  nodes[0]->setLft(nodes[1]);
+  nodes[1]->setAnc(nodes[0]);
+  
+  std::set<Node*>     activeNodes;
+  activeNodes.insert(nodes[1]);
+  root = nodes[0];
+  
+  /* generate the full tree */
+  double t = 0.0;
+  while(t < duration){
+    
+    /* increment t using exponential distribution */
+    double rate = activeNodes.size() * (lambda + mu);
+    t += prngPtr->get_rexp(rate);
+    
+    if(t < duration){
+      /* choose a node */
+      Node* p = nullptr;
+      
+      /* choose type of event */
+      double u = prngPtr->get_runif();
+      if(u < lambda / (lambda + mu)){
+        /* speciation */
+         
+      } else {
+        /* extinction */
+        
+      }
+      
+    }
+    
+  }
 }
 
 Tree::~Tree(){
